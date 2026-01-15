6003bf8c416a189e714c8f92e9f28e218bb0e55b
apaulliao <apaul.liao@gmail.com>
2026-01-15 12:16:17 +0800

    Update vite.config.js

--- Classroom Tools/vite.config.js
+++ Classroom Tools/vite.config.js
@@ -7,5 +7,6 @@ export default defineConfig({
   // 重要：請將 'your-repo-name' 改成您在 GitHub 上建立的儲存庫名稱 (Repository Name)
   // 例如：如果您的網址是 https://username.github.io/my-classroom/
   // 這裡就要填 '/my-classroom/'
-  base: 'schooltool', 
+  base: '/schooltool/', 
+
 })
